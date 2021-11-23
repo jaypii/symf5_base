6057c533d082e0c0843553f3cca1e2724d246961
jaypii <jpomv2@icloud.com>
2021-11-23 15:57:17 +0100

    new Symfony Base App

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    #[Route('/blog', name: 'blog')]
+    public function index(): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+    /**
+     * @Route("/blog", name="blog_list")
+     */
+    public function list()
+    {
+       return new Response('<h2>Blog List</h2>');
+    }
+}
